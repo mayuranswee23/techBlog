09f685377d98f10b337bf27a448580f786cce838
Mayuran <mayuranswee23@gmail.com>
2021-08-23 16:44:46 -0400

    added js for functionality

---
+++ public/javascript/createpost.js
@@ -0,0 +1,25 @@
+async function newPostHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/post', {
+        method: 'post',
+        body: JSON.stringify({
+          title,
+          content
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  document.querySelector('#save-post-button').addEventListener('submit', newPostHandler);
---
+++ public/javascript/updatepost.js
@@ -0,0 +1,43 @@
+async function updatePostHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const body = document.querySelector('input[name="content"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${post_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  async function deletePostHandler(event) {
+    event.preventDefault();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1];
+
+     const response = await fetch(`/api/posts/${post_id}`, {
+            method: 'DELETE',
+     });
+
+     if (response.ok) {
+        document.location.replace('/');
+     }
+  };
+
+  document.querySelector('#save-post-btn').addEventListener('submit', updatePostHandler);
+  document.querySelector('#delete-post-btn').addEventListener('submit', deletePostHandler);
